4a4b92b679996e78a4514194c8f8d8f6cc984cc8
Yixin Zhang <yizhang@f1r5s1.codam.nl>
2023-10-17 18:41:05 +0200

    ok

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/09 15:17:48 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/10/16 11:16:39 by svan-has      ########   odam.nl         */
+/*   Updated: 2023/10/17 18:41:01 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,32 @@
 
 #include "lib/mlx42/include/MLX42/MLX42.h"
 #include <stdio.h>
+
+typedef struct s_color
+{
+    double  r;
+    double  g;
+    double  b;
+}t_color;
+
+typedef struct s_vec
+{
+    double  x;
+    double  y;
+    double  z;
+}t_vec;
+
+typedef struct s_ray
+{
+    t_vec   orig;
+    t_vec   dir;
+}t_ray;
+
+typedef struct s_camera
+{
+    t_vec   position;
+    double  ratio;
+}t_camera;
 
 void    parse_input(int argc, char **argv);
 void    error_exit(void);
