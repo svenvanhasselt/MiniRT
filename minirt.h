0de37d222e0ebb75c6e047c1e1493b8422e6de43
Sven Van Hasselt <svan-has@f1r5s5.codam.nl>
2023-10-18 10:33:50 +0200

    fix

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/09 15:17:48 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/10/16 11:16:39 by svan-has      ########   odam.nl         */
+/*   Updated: 2023/10/18 10:31:55 by svan-has      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 #include "lib/mlx42/include/MLX42/MLX42.h"
 #include <stdio.h>
+
+enum   t_type
+{
+    sphere,
+    cilinder,
+};
+
+typedef struct s_object
+{
+    int id;
+    
+}   t_object;
 
 void    parse_input(int argc, char **argv);
 void    error_exit(void);
