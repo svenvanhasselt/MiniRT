f6a9470870f44d073f782c564bcfbfc59b899289
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-10-18 10:38:23 +0200

    ok

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/09 15:17:48 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/10/17 18:41:01 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/10/18 10:02:14 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,8 @@ typedef struct s_camera
 void    parse_input(int argc, char **argv);
 void    error_exit(void);
 void    *null_check(void *check);
+
+//yizhang
+t_vec set_vec(double x, double y, double z);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,32 +6,52 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/09 17:46:14 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/10/16 11:13:41 by svan-has      ########   odam.nl         */
+/*   Updated: 2023/10/18 10:13:27 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
 
-//open window
-//give some color
-//sending rays into the scene
-//vector?
-//
+t_vec set_vec(double x, double y, double z)
+{
+	t_vec new;
+	new.x = x;
+	new.y = y;
+	new.z = z;
+	return (new);	
+}
+
+t_ray set_ray(t_vec orig, t_vec dir)
+{
+	t_ray new;
+	new.orig = orig;
+	new.dir = dir;
+	return (new);
+}
 
 int main(int argc, char **argv)
 {
 	mlx_t* mlx;
 	mlx_image_t *img;
+	double focal_length = 1.0;
+	double viewport_heigh = 2.0;
+	double viewport_weith = 2.0;
+	
+	t_vec camera = set_vec(0,0,0);
+	
+	t_vec viewport_u = set_vec(0,0, focal_length);
+	t_vec viewport_v = set_vec(0,0, focal_length);
+	
+	//parse_input(argc, argv);
 
-	parse_input(argc, argv);
+	mlx = mlx_init(80, 40, "MiniRT", true);
+	img = mlx_new_image(mlx, 80, 40);
 
-	mlx = mlx_init(1600, 800, "MiniRT", true);
-	img = mlx_new_image(mlx, 1600, 800);
 	//print image with color
 	mlx_image_to_window(mlx, img, 0, 0);
-	for(int j = 0; j < 1600; j++)
+	for(int j = 0; j < 80; j++)
 	{
-		for(int i = 0; i < 800; i++)
+		for(int i = 0; i < 40; i++)
 			mlx_put_pixel(img, j, i, 0xFFFFFFFF);
 	}
 
