9a5c64f7bc6f4b46f6b5cca1e45b9bed2cad77e7
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-12-05 15:13:57 +0100

    optimizated the hit plan

--- src/intersection/hit_plane.c
+++ src/intersection/hit_plane.c
@@ -6,29 +6,23 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/27 09:24:28 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/12/01 11:06:49 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/12/05 15:04:32 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minirt.h"
 
-/* 
-plane equation is :p*n -d = 0
-ray: p(t) = s + tc;
-intersection : (s +tc) * n -d = 0
-t = (d - s*n)/c*n
+/*
+	(D*t+X)|V = 0
+	D|V*t = -X|V
+	t = -X|V / D|V
+	https://hugi.scene.org/online/hugi24/coding%20graphics%20chris%20dragan%20raytracing%20shapes.htm
  */
 bool	hit_plane(t_object *obj, t_ray *ray)
 {
-	float   ortho;
-    float   d;
-    float   dis;
-    
-    ortho = dot(obj->vec2, ray->norm);
-    if (ortho == 0)
-        return (false);
-    d = dot(obj->vec, obj->vec2);
-    dis = dot(obj->vec2, ray->orig);
-    obj->t = (d - dis) / ortho;
+	t_vec	oc;
+	
+	oc = sub(ray->orig, obj->vec);
+	obj->t = -dot(oc,obj->vec2)/ dot(ray->dir,obj->vec2);
 	return (true);
 }
